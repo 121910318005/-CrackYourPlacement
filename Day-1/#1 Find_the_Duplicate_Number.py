621e5b43ec17f4a32f77ea91789d00c09f83280d
G V Gowtham <72001670+121910318005@users.noreply.github.com>
2022-05-20 16:25:20 +0530

    Update #1 Find_the_Duplicate_Number.py
    ![image](https://user-images.githubusercontent.com/72001670/169513643-6f5d7ee4-47f4-4dc9-b8c9-0422c64c2bb8.png)

--- Day-1/#1 Find_the_Duplicate_Number.py
+++ Day-1/#1 Find_the_Duplicate_Number.py
@@ -1,18 +1,37 @@
 '''
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+ 
+
+Constraints:
+
+1 <= n <= 105
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times.
+'''
+
 #Leet code solution
+#Below code is not optimal because they asked not to modify the array.
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         nums.sort()
         for i in range(1, len(nums)):
             if nums[i-1]^nums[i]==0:
                 return nums[i]
- '''             
-
-nums = list(map(int, input().split(' ')))
-nums.sort()
-for i in range(1, len(nums)):
-  if nums[i-1]==nums[i]:
-    print(f'Duplicate number is: {nums[i]}')
-    break
-else:
-  print('Duplicate number not found')
+              
+#Optimal code with O(n) Time complexity, without changing/ modifying the array.
+''' Given that array contains numbers within range of 1 to n-1, where n is the length of the array. Here it is 1 <= nums[i] <= n'''
+''' So  
