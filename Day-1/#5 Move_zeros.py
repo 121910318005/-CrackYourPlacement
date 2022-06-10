332b80e836a7cc416ba6aea7041c02971f033058
G V Gowtham <72001670+121910318005@users.noreply.github.com>
2022-06-11 04:58:13 +0530

    Update #5 Move_zeros.py

--- Day-1/#5 Move_zeros.py
+++ Day-1/#5 Move_zeros.py
@@ -1,17 +1,25 @@
 '''
-#Leet code
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        temp=[]
-        c=0
-        for i in range(len(nums)):
-            if nums[i]!=0:
-                temp.append(nums[i])
-            else:
-                c+=1
-        for i in range(c):
-            temp.append(0)
-        for i in range(len(temp)):
-            nums[i]=temp[i]
-        del temp
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
 '''
+
+# Without using extra space.
+# Use two pointer approach
+i, j = 0, 0
+while j<len(nums):
+    if nums[j]!=0:
+        nums[i], nums[j] = nums[j], nums[i]
+        i+=1
+        j+=1
+    else:
+        j+=1
+print(nums)
