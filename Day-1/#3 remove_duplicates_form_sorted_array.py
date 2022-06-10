c7fc436e6f65d4abb1033ed460cbfa354f5642cf
G V Gowtham <72001670+121910318005@users.noreply.github.com>
2022-06-10 05:54:06 +0530

    Update #3 remove_duplicates_form_sorted_array.py

--- Day-1/#3 remove_duplicates_form_sorted_array.py
+++ Day-1/#3 remove_duplicates_form_sorted_array.py
@@ -1,40 +1,55 @@
 '''
-#Leet code
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. 
+More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. 
+It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+'''
+
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i=1
-        while i<len(nums):
-            if nums[i-1]^nums[i]==0:
-                del nums[i]
-            else:
+        i, j = 1, 1
+        while j<len(nums):
+            if nums[j-1]!=nums[j]:
+                nums[i] = nums[j]
                 i+=1
-'''
-
-'''
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        temp = []
-        res=1
-        temp.append(nums[0])
-        for i in range(1, len(nums)):
-            if (nums[i]^temp[res-1])!=0:
-                temp.append(nums[i])
-                res+=1
-        for i in range(res):
-            nums[i]=temp[i]
-        del temp
-        return res
-'''
-
-nums = list(map(int, input().split(' ')))
-temp = []
-count = 1
-temp.append(nums[0])
-for i in range(1, len(nums)):
-  if (nums[i]^temp[count-1])!=0:
-    temp.append(nums[i])
-    count+=1
-for i in range(res):
-  nums[i] = temp[i]
-del temp
-print(f'k = {count}, nums = {nums}')
+                j+=1
+            else:
+                j+=1
+        return i
