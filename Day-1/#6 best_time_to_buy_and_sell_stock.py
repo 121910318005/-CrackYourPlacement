71fe9f102cc7cc15d529b8800dfd7d4502d67500
G V Gowtham <72001670+121910318005@users.noreply.github.com>
2022-06-11 05:19:39 +0530

    Update #6 best_time_to_buy_and_sell_stock.py

--- Day-1/#6 best_time_to_buy_and_sell_stock.py
+++ Day-1/#6 best_time_to_buy_and_sell_stock.py
@@ -1,5 +1,5 @@
 '''
-#Leet code
+#Leet code Approach-1
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_profit, max_profit = float('inf'), 0
@@ -18,3 +18,18 @@ for i in prices:
   diff = i-min_prof
   max_prof=max(diff, max_profit)
 print('Max profit gained: ',max_profit)
+
+
+# Approach-2
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        i, j = 0,1
+        m=0
+        while j<len(prices):
+            if prices[i]>prices[j]:
+                i = j
+            else:
+                if prices[j]-prices[i]>m:
+                    m = prices[j]-prices[i]
+            j+=1
+        return m
